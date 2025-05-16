7aba9ca7a44ada3adac462bf13b1811e2e69f0f2
Maxim <maximalexandermarchal@gmail.com>
2025-05-16 17:12:14 +0200

    chore: install daisyUI

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <>
+        <button className="btn btn-primary">One</button>
+        <button className="btn btn-secondary">Two</button>
+        <button className="btn btn-accent">Three</button>
+        </>
+    )
+}
